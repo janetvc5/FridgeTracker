2e3dc10728aa4209b5a26f0c772440daebdfe897
haileykg7 <haileygriffith7@gmail.com>
2019-01-29 16:37:46 -0600

    expanding hello world

--- Experiments/Hailey/HelloWorld2/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Experiments/Hailey/HelloWorld2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
         //test comment
+
+        b1=(Button) findViewById(R.id.buttonSayHello);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b2=(Button) findViewById(R.id.buttonDone);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, ThankYou.class);
+                startActivity(i);
+            }
+        });
     }
 }
