9d2385255f1f2169d3cf37c93d361f6705a310a0
Madison <jacobsen@iastate.edu>
2019-02-21 17:07:21 -0600

    FT quick

--- fridge_tracker/app/src/main/java/com/example/fridge_tracker/MainActivity.java
+++ fridge_tracker/app/src/main/java/com/example/fridge_tracker/MainActivity.java
@@ -2,12 +2,195 @@ package com.example.fridge_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    Button sendButton, getButton;
+
+    EditText edit1, edit2, edit3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendButton = (Button) findViewById(R.id.sendbutton);
+        getButton = (Button) findViewById(R.id.getbutton);
+        edit1 = (EditText) findViewById(R.id.sendtext);
+        edit2 = (EditText) findViewById(R.id.sendtext);
+        edit3 = (EditText) findViewById(R.id.sendtext);
+
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    sendJson("5");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getJson(String.valueOf(edit1.getText()));
+            }
+        });
+
+    }
+
+    void getJson(String userID)
+    {
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        String url = "http://cs309-af-1.misc.iastate.edu:8080/user/" + userID;
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, new JSONObject(),
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String fName = response.getString("role");
+                            String lName = String.valueOf(response.getInt("fridgeid"));
+
+
+                            Log.d("Role: ",fName + "  Fridge for user: " + lName);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+              error.printStackTrace();
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                //headers.put("fridgeid", "role");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("fridgeid", "36");
+                params.put("role", "abc@androidhive.info");
+
+
+                return params;
+            }
+
+        };
+
+        mQueue.add(jsonObjReq);
+
+    }
+
+    void sendJson(String userID) throws JSONException {
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        String url = "http://cs309-af-1.misc.iastate.edu:8080/user/new";
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, getJs(),
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try
+                        {
+                            Log.d("Response from server: ", response.getString("fridgeid"));
+                        }
+                        catch (JSONException e)
+                        {
+
+                        }
+
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+//            @Override
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("fridgeid", "34");
+//                params.put("role", "general");
+//
+//                return params;
+//            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("fridgeid", "34");
+                params.put("role", "general");
+
+                return params;
+            }
+
+
+
+        };
+
+        mQueue.add(jsonObjReq);
+
+    }
+
+
+    protected JSONObject getJs() throws JSONException {
+        JSONObject params = new JSONObject();
+        params.put("fridgeid", 34);
+        params.put("role", "general");
+
+        return params;
     }
 }
