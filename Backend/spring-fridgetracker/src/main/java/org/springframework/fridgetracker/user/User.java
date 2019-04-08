2d436983506b9a37cc39c1076e089efd61fec44b
Joshua Flory <jwflory@iastate.edu>
2019-04-07 22:26:12 -0500

    Edited Omitted Json Properties, and made Recipe Class

--- Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/user/User.java
+++ Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/user/User.java
@@ -43,7 +43,7 @@ public class User {
 	private Integer role;
 	
 	@ManyToOne(targetEntity = Fridge.class)
-	@JsonIgnoreProperties({"users","fridgecontents"})
+	@JsonIgnoreProperties({"users","fridgecontents","grocerylist"})
 	@JoinColumn
 	private Fridge fridge;
 
