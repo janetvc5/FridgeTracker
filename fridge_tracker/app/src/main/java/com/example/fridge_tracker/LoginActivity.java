79087a73bdcd1db330cffd65d53481df60b78bd5
haileykg7 <haileygriffith7@gmail.com>
2019-04-09 13:03:12 -0500

    adding login work on master

--- fridge_tracker/app/src/main/java/com/example/fridge_tracker/LoginActivity.java
+++ fridge_tracker/app/src/main/java/com/example/fridge_tracker/LoginActivity.java
@@ -9,6 +9,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button login;
@@ -17,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     TextView title;
     TextView attempts;
     int counter = 5;
+    String loggeduser;
+    ArrayList<String> loggedInUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,253 +56,164 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void validate(String username, String password){
+    private void validate(String username, String password) {
 
-        if((username.equals("user2")) && (password.equals("pass")))
-        {
-            Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
+        if ((username.equals("user2")) && (password.equals("pass"))) {
+            Intent intent = new Intent(LoginActivity.this, SearchActivity.class);
             startActivity(intent);
-        }else {
+        } else {
             counter--;
 
-            attempts.setText("Login attempts remaining: "+String.valueOf(counter));
+            attempts.setText("Login attempts remaining: " + String.valueOf(counter));
 
             if (counter == 0) {
                 login.setEnabled(false);
             }
         }
     }
+
+    String[] userResult;
+    String[] passResult;
+
+
+    //test below!!!!
+    private void getUser(String uValue, String pValue) throws JSONException {
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        // prepare a result array
+
+        String url="http://cs309-af-1.misc.iastate.edu:8080/user";
+        JsonArrayRequest userArrayReq=new JsonObjectRequest(Request.Method.GET,
+                url, new JSONObject(),
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        userResult=response.getJSONArray("username");
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(userArrayReq);
+
+        int i=0;
+
+        // loop through all of our users
+        while(userResult[i]!=null){
+            // get the user we are iterating through now
+            String user = userResult[i];
+
+            // check if the user has the specified property
+            if (user == uValue) {
+                JsonArrayRequest passArrayReq=new JsonObjectRequest(Request.Method.GET,
+                        url, new JSONObject(),
+                        new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                passResult=response.getJSONArray("password");
+
+                            }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+                int j=0;
+                while(j<passResult.length)
+                {
+                    String pass=passResult[j];
+                    if (pass==pValue);
+                }
+                mQueue.add(passArrayReq);
+            }
+        }
+
+        // return the results or null, if nothing was found (for single match search)
+
+        return ;
+
+    }
+
+
+    /**
+     * boolean|Object login ( string username, string password )
+     * <p>
+     * Provides the functionality to be able to log in on a user.
+     *
+     * @param username Username of the user to log in on.
+     * @param password Password of the user to log in on.
+     * @return boolean|Object Returns the user object, or false, if login was not successful.
+     */
+    private void login(String username, String password) {
+        // checks whether username and password have been filled in
+        if (username.length() > 0 && password.length() > 0) {
+            // prepare a variable to store the user object, if any is received
+            //String loggeduser;
+
+            // server should handle everything below...
+            // iterate through all users in the 'users' array (or database table perhaps, on server-side)
+            for (int i=0; i<userResult.length; i++){
+                // grab the property value with the property
+                String user = userResult[i];
+
+                // check if username and password match
+                if (username == user && password == password)
+                    // set value of 'loggeduser' to the property value (user)
+                    loggeduser = user;
+            }
+                //add user to the arraylist of logged in users
+                loggedInUsers.add(loggeduser);
+
+        }
+
+        //return false;
+    }
+
+    /**
+     * boolean logout ( number userid )
+     * <p>
+     * Provides the functionality to be able to log out from a user.
+     *
+     * @param number userid ID of the user to log out of.
+     * @return boolean Returns a boolean representing whether the log out was successful or not.
+     *
+    function logout(userid) {
+        // check whether the ID is actually logged in
+        if (loggedusers[userid]) {
+            // temporary array, which we will be filling
+            var temporary = [];
+
+            // let's loop through logged users
+            for (var id in loggedusers)
+            // ignore our user
+            if (id != userid)
+                // let's put this user to the array
+                temporary[id] = true;
+
+            // we replace the 'loggedusers' array with our new array
+            loggedusers = temporary;
+
+            // update the logged in list
+            updatelist();
+
+            // we have successfully logged out
+            return true;
+        }
+
+        // we have not successfully logged out
+        return false;
+    }
+*/
+
 }
-//
-////test below!!!!
-//function getUserByProperty(key, value, strict, multiple, case_insensitive) {
-//    // prepare a result array
-//    var result = [];
-//
-//    // loop through all of our users
-//    for (var index in users) {
-//        // get the user we are iterating through now
-//        var user = users[index];
-//
-//        // check if the user has the specified property
-//        if (typeof user[key] != 'undefined') {
-//            // get the property value
-//            var compare = user[key];
-//
-//            // doing something case insensitive
-//            if (case_insensitive) {
-//                // if the property value is a string
-//                if (typeof compare == 'string')
-//                // we want to turn it to lower case
-//                compare = compare.toLowerCase();
-//
-//                // if the specified value is a string
-//                if (typeof value == 'string')
-//                // we want to turn it to lower case
-//                value = value.toLowerCase();
-//            }
-//
-//            // if specified value is not defined, or values match
-//            if (typeof value == 'undefined' || ((strict && compare === value) || (!strict && compare == value))) {
-//                // if we want multiple results
-//                if (multiple) {
-//                    // the result will be appended to the result array
-//                    result.push(user);
-//                } else {
-//                    // otherwise we just return it
-//                    return user;
-//                }
-//            }
-//        }
-//    }
-//
-//    // return the results or null, if nothing was found (for single match search)
-//    return multiple ? result : null;
-//}
-//
-//    /**
-//     * null|Object getUserById ( number id )
-//     *
-//     * Gets a user with the specified ID.
-//     *
-//     * @param number id ID of user to get.
-//     *
-//     * @return null|Object Returns the user object, or null, if not found.
-//     */
-//    function getUserById(id) {
-//        return getUserByProperty('id', id);
-//    }
-//
-//    /**
-//     * null|Object getUserByUsername ( string username [ , boolean case_insensitive = false ] )
-//     *
-//     * Gets a user with the specified username.
-//     *
-//     * @param string username Username of user to get.
-//     * @param boolean case_insensitive Should character case be ignored?
-//     *
-//     * @return null|Object Returns the user object, or null, if not found.
-//     */
-//    function getUserByUsername(username, case_insensitive) {
-//        return getUserByProperty('username', username, false, false, case_insensitive);
-//    }
-//
-//    /**
-//     * boolean|array getUsersByType ( string type [ , boolean case_insensitive = false ] )
-//     *
-//     * Gets all users with the specified type.
-//     *
-//     * @param string type Type of user to look for.
-//     * @param boolean case_insensitive Should character case be ignored?
-//     *
-//     * @return array Returns the an array of user objects.
-//     */
-//    function getUsersByType(type, case_insensitive) {
-//        return getUserByProperty('type', type, false, true, case_insensitive);
-//    }
-//
-//    /**
-//     * boolean|Object login ( string username, string password )
-//     *
-//     * Provides the functionality to be able to log in on a user.
-//     *
-//     * @param string username Username of the user to log in on.
-//     * @param string password Password of the user to log in on.
-//     *
-//     * @return boolean|Object Returns the user object, or false, if login was not successful.
-//     */
-//    function login(username, password) {
-//        // checks whether username and password have been filled in
-//        if (typeof username == 'string' && typeof password == 'string' && username.length > 0 && password.length > 0) {
-//            // prepare a variable to store the user object, if any is received
-//            var loggeduser;
-//
-//            // server should handle everything below...
-//            // iterate through all users in the 'users' array (or database table perhaps, on server-side)
-//            for (var index in users) {
-//                // grab the property value with the property
-//                var user = users[index];
-//
-//                // check if username and password match
-//                if (username === user.username && password === user.password)
-//                    // set value of 'loggeduser' to the property value (user)
-//                    loggeduser = user;
-//            }
-//            // ... up to this point, and the user returned from the server should be set in to 'loggeduser'
-//            // make sure highly sensitive information is not returned, such as hash, salt or anything
-//
-//            // check whether the user is set
-//            if (typeof loggeduser != 'undefined') {
-//                // save the ID of the user to the 'loggedusers' array
-//                loggedusers[loggeduser.id] = true;
-//
-//                // update the logged in list
-//                updatelist();
-//
-//                // return the received user object
-//                return loggeduser;
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    /**
-//     * boolean logout ( number userid )
-//     *
-//     * Provides the functionality to be able to log out from a user.
-//     *
-//     * @param number userid ID of the user to log out of.
-//     *
-//     * @return boolean Returns a boolean representing whether the log out was successful or not.
-//     */
-//    function logout(userid) {
-//        // check whether the ID is actually logged in
-//        if (loggedusers[userid]) {
-//            // temporary array, which we will be filling
-//            var temporary = [];
-//
-//            // let's loop through logged users
-//            for (var id in loggedusers)
-//            // ignore our user
-//            if (id != userid)
-//                // let's put this user to the array
-//                temporary[id] = true;
-//
-//            // we replace the 'loggedusers' array with our new array
-//            loggedusers = temporary;
-//
-//            // update the logged in list
-//            updatelist();
-//
-//            // we have successfully logged out
-//            return true;
-//        }
-//
-//        // we have not successfully logged out
-//        return false;
-//    }
-//
-//    /**
-//     * boolean updatelist ( void )
-//     *
-//     * Provides the functionality to update the #logged-in-list element
-//     * with the logged in users names and logout links.
-//     *
-//     * @return boolean Returns a boolean representing whether the update was successful or not.
-//     */
-//    function updatelist() {
-//        // get the #logged-in-list element
-//        var list_element = document.getElementById('logged-in-list');
-//
-//        // check the element exists
-//        if (list_element) {
-//            // get the #logged-in element
-//            var list_container_element = document.getElementById('logged-in');
-//
-//            // check the element exists and that we should be changing the styles
-//            if (list_container_element)
-//                // if there are no logged in users, "hide" the element, otherwise "show" it
-//                list_container_element.style.visibility = loggedusers.length === 0 ? 'hidden' : 'visible';
-//
-//            // we take the first child with a while loop
-//            while (list_element.firstChild)
-//                // remove the child, and it will repeat doing so until there is no firstChild left for the list_element
-//                list_element.removeChild(list_element.firstChild);
-//
-//            // we loop through every logged in user
-//            for (var id in loggedusers) {
-//                // get the user by ID
-//                var user = getUserById(id);
-//
-//                // check if that user is a user
-//                if (user) {
-//                    // we create necessary elements to cover our logout functionality
-//                    var p = document.createElement('P');
-//                    p.innerText = user.username;
-//                    var a = document.createElement('A');
-//                    a.userid = id;
-//                    a.href = '#';
-//                    a.innerHTML = '(logout)';
-//
-//                    // we bind an onclick event listener to the link
-//                    a.addEventListener('click', function(e) {
-//                        e.preventDefault();
-//
-//                        // we will now execute the logout function for this user ID
-//                        logout(e.srcElement.userid);
-//                    });
-//
-//                    // we append the link to the paragraph element
-//                    p.appendChild(a);
-//
-//                    // we append the paragraph to the list element
-//                    list_element.appendChild(p);
-//                }
-//            }
-//
-//            return true;
-//        }
-//
-//        return false;
-//    }
