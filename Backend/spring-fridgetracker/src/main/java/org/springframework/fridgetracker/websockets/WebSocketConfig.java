358eb5e5a8d484dea4fe2eba90001045aa4384f1
Joshua Flory <jwflory@iastate.edu>
2019-04-08 23:33:14 -0500

    Removed WebSocketConfig to help with compiling

--- Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/websockets/WebSocketConfig.java
+++ Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/websockets/WebSocketConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration 
-public class WebSocketConfig {  
+public class WebSocketConfig {  /*
     @Bean  
     public ServerEndpointExporter serverEndpointExporter(){  
         return new ServerEndpointExporter();  
-    }  
+    }  */
 } 
