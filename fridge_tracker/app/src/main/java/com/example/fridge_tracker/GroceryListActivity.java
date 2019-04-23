48beccd1c4f751cb9a1c5dc09e063699ff673314
haileykg7 <haileygriffith7@gmail.com>
2019-04-23 18:19:30 -0500

    javadocs for adapter

--- fridge_tracker/app/src/main/java/com/example/fridge_tracker/GroceryListActivity.java
+++ fridge_tracker/app/src/main/java/com/example/fridge_tracker/GroceryListActivity.java
@@ -6,23 +6,15 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 
 import android.content.Intent;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.ActionBar;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
+
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.PopupMenu;
-import android.widget.Toast;
-import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
