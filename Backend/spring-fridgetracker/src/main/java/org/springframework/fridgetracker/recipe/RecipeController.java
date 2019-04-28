fb68998805cb9648edda2e44b2fc2d32a4572f7c
Janet <janetv@iastate.edu>
2019-04-27 22:11:20 -0500

    sorting of recipes

--- Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/recipe/RecipeController.java
+++ Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/recipe/RecipeController.java
@@ -1,32 +1,94 @@
 package org.springframework.fridgetracker.recipe;
-
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.websocket.server.PathParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.fridgetracker.fridge.Fridge;
+import org.springframework.fridgetracker.fridge.FridgeRepository;
+import org.springframework.fridgetracker.fridgecontents.Fridgecontents;
 import org.springframework.fridgetracker.recipeitem.Recipeitem;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class RecipeController {
 	private final Logger logger = LoggerFactory.getLogger(RecipeController.class);
+	@Autowired
+	RecipeRepository recipeRepository;
+	@Autowired
+	FridgeRepository fridgeRepository;
+	
 	
 	@RequestMapping(method= RequestMethod.GET, path="/recipes")
 	public List<Recipe> getAllRecipes() {
-		//Returning this for mockito test
-		return new ArrayList();
+		return recipeRepository.findAll();
+	}
+	
+	/**
+	 * Returns all recipes in the database on a GET call to "{url}/recipes"
+	 * @return ArrayList of recipes
+	 */
+	@RequestMapping(method= RequestMethod.POST, path="/recipes/new")
+	public Map<String, String> addRecipe(@RequestBody Recipe recipe) {
+		Map<String,String> ret = new HashMap<String,String>();
+		if(recipe==null) {
+			ret.put("success", "false");
+			return ret;
+		}
+		recipeRepository.save(recipe);
+		ret.put("success", "true");
+		return ret;
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, path="/recipes/{id}")
 	public Optional<Recipe> getRecipeById(@PathParam("id") Integer id) {
 		// return this for mockito
-		return Optional.empty();
+		return recipeRepository.findById(id);
+	}
+	
+	/**
+	 * Returns a recipe with the given ID on a GET call to "{url}/recipes/{id}"
+	 * @param id - The id of the Recipe being retrieved (part of url)
+	 * @return An optional - If the recipe exists, it will have a value. Else, it will have nothing
+	 */
+	@RequestMapping(method=RequestMethod.GET, path="/recipes/pantry/{id}")
+	public Map<String,Object> sortedRecipeList(@PathParam("id") Integer id) {
+		Map<String,Object> ret = new HashMap<String,Object>();
+		Optional<Fridge> f = fridgeRepository.findById(id);
+		if(!f.isPresent()) {
+			ret.put("success", (Object)"false");
+			ret.put("reason", (Object) "fridge does not exist");
+		}
+		List<Recipe> recipes = recipeRepository.findAll();
+		Iterator<Recipe> i = recipes.iterator();
+		Recipe r;
+		ArrayList<Map<String,Object>> vals = new ArrayList<Map<String,Object>>();
+		while(i.hasNext()) {
+			r = i.next();
+			HashMap<String,Object> newVal = new HashMap<String,Object>();
+			newVal.put("recipe", (Object)r);
+			newVal.put("rank", (Object) RecipeUtility.percentOfIngredients(r.getItems(),f.get().getFridgecontents()));
+			vals.add(newVal);
+		}
+		Collections.sort(vals, (r1, r2) -> {
+			return ((double)r1.get("rank")) < ((double)r2.get("rank")) ? -1
+					:((double)r1.get("rank")) > ((double)r2.get("rank")) ? 1
+					: 0;
+					
+		});
+		ret.put("recipes", (Object)vals);
+		return ret;
 	}
 }
--- Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/recipe/RecipeUtility.java
+++ Backend/spring-fridgetracker/src/main/java/org/springframework/fridgetracker/recipe/RecipeUtility.java
@@ -1,11 +1,20 @@
 package org.springframework.fridgetracker.recipe;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.fridgetracker.fridgecontents.Fridgecontents;
 import org.springframework.fridgetracker.recipeitem.Recipeitem;
 
 public class RecipeUtility {
+	/**
+	 * Using a given recipe ID, controller, and ingredient, this function will determine if the given ingredient has the same name as an ingredient in the recipe ingredients
+	 * @param con - Recipe Controller used to request Recipeitem list
+	 * @param id - ID of the recipe in question
+	 * @param ri - Recipe item to be tested for it's presence
+	 * @return True if ri is in the ingredients for the recipe, and the recipe exists, false otherwise
+	 */
 	public static boolean isInRecipe(RecipeController con, Integer id, Recipeitem ri) {
 		Optional<Recipe> recO = con.getRecipeById(id);
 		if(!recO.isPresent()) return false;
@@ -17,4 +26,24 @@ public class RecipeUtility {
 		}
 		return false;
 	}
+
+	public static double percentOfIngredients(List<Recipeitem> ingr, List<Fridgecontents> fridge) {
+		double denom = ingr.size();
+		double num = 0;
+		Iterator<Recipeitem> i = ingr.iterator();
+		double a;
+		Iterator<Fridgecontents> j;
+		Recipeitem b;
+		while(i.hasNext()) {
+			a=0;
+			b = i.next();
+			j = fridge.iterator();
+			while(a==0 && j.hasNext()) {
+				if(j.next().getFoodname().equals(b.getItemname())) a++;
+			}
+			num+=a;
+		}
+		return (num/denom);
+	}
+
 }
